e33c4923ac326350e1d970c9da02cb24985b9da2
ignatiusfairlight <lawaierang@gmail.com>
2025-07-17 19:06:50 +0800

    Added responsive menu for mobile

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
 	import '../app.css';
-	import * as NavigationMenu from "$lib/components/ui/navigation-menu/index.js";
+	import * as NavigationMenu from '$lib/components/ui/navigation-menu/index.js';
+	import * as Drawer from '$lib/components/ui/drawer/index.js';
+	import { Button } from '$lib/components/ui/button/index.js';
 	import { navigationMenuTriggerStyle } from '$lib/components/ui/navigation-menu/navigation-menu-trigger.svelte';
 	import { onNavigate } from '$app/navigation';
+	import { MediaQuery } from 'svelte/reactivity';
+	import DrawerClose from '$lib/components/ui/drawer/drawer-close.svelte';
 
 	onNavigate((navigation) => {
 		if (!document.startViewTransition) return;
@@ -15,42 +19,66 @@
 		});
 	});
 
+	const isDesktop = new MediaQuery('(min-width: 768px)');
+
 	let { children } = $props();
 </script>
 
-<div class="flex flex-col justify-center items-end mr-10 mt-5">
-	<NavigationMenu.Root>
-		<NavigationMenu.List>
-			<NavigationMenu.Item>
-				<NavigationMenu.Link>
-					{#snippet child()}
-						<a href="./" class={navigationMenuTriggerStyle()}>Home</a>
-					{/snippet}
-				</NavigationMenu.Link>
-			</NavigationMenu.Item>
-			<NavigationMenu.Item>
-				<NavigationMenu.Link>
-					{#snippet child()}
-						<a href="about" class={navigationMenuTriggerStyle()}>About Me</a>
-					{/snippet}
-				</NavigationMenu.Link>
-			</NavigationMenu.Item>
-			<NavigationMenu.Item>
-				<NavigationMenu.Link>
-					{#snippet child()}
-						<a href="projects" class={navigationMenuTriggerStyle()}>Projects</a>
-					{/snippet}
-				</NavigationMenu.Link>
-			</NavigationMenu.Item>
-			<NavigationMenu.Item>
-				<NavigationMenu.Link>
-					{#snippet child()}
-						<a href="contact" class={navigationMenuTriggerStyle()}>Contact</a>
-					{/snippet}
-				</NavigationMenu.Link>
-			</NavigationMenu.Item>
-		</NavigationMenu.List>
-	</NavigationMenu.Root>
-</div>
+{#if isDesktop.current}
+	<div class="mt-5 mr-10 flex flex-col items-end justify-center">
+		<NavigationMenu.Root>
+			<NavigationMenu.List>
+				<NavigationMenu.Item>
+					<NavigationMenu.Link>
+						{#snippet child()}
+							<a href="./" class={navigationMenuTriggerStyle()}>Home</a>
+						{/snippet}
+					</NavigationMenu.Link>
+				</NavigationMenu.Item>
+				<NavigationMenu.Item>
+					<NavigationMenu.Link>
+						{#snippet child()}
+							<a href="about" class={navigationMenuTriggerStyle()}>About Me</a>
+						{/snippet}
+					</NavigationMenu.Link>
+				</NavigationMenu.Item>
+				<NavigationMenu.Item>
+					<NavigationMenu.Link>
+						{#snippet child()}
+							<a href="projects" class={navigationMenuTriggerStyle()}>Projects</a>
+						{/snippet}
+					</NavigationMenu.Link>
+				</NavigationMenu.Item>
+				<NavigationMenu.Item>
+					<NavigationMenu.Link>
+						{#snippet child()}
+							<a href="contact" class={navigationMenuTriggerStyle()}>Contact</a>
+						{/snippet}
+					</NavigationMenu.Link>
+				</NavigationMenu.Item>
+			</NavigationMenu.List>
+		</NavigationMenu.Root>
+	</div>
+{:else}
+	<div class="ml-5 pt-5">
+		<Drawer.Root direction="left">
+			<Drawer.Trigger>Menu</Drawer.Trigger>
+			<Drawer.Content class="flex flex-col justify-center items-start gap-10 ml-5">
+				<Drawer.Close>
+					<a href="./" class={navigationMenuTriggerStyle()}>Home</a>
+				</Drawer.Close>
+				<DrawerClose>
+					<a href="about" class={navigationMenuTriggerStyle()}>About Me</a>
+				</DrawerClose>
+				<Drawer.Close>
+					<a href="projects" class={navigationMenuTriggerStyle()}>Projects</a>
+				</Drawer.Close>
+				<Drawer.Close>
+					<a href="contact" class={navigationMenuTriggerStyle()}>Contact</a>
+				</Drawer.Close>
+			</Drawer.Content>
+		</Drawer.Root>
+	</div>
+{/if}
 
 {@render children()}
