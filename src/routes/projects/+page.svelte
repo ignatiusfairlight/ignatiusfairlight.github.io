441ad7b2231c89ec81cb04298065204b03d54569
Bryan Erang Lawai <138428485+ignatiusfairlight@users.noreply.github.com>
2026-01-02 01:16:37 +0800

    Added scrollability to the contents in Resume and Projects

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -3,78 +3,146 @@
 </script>
 
 <main
-	class="mx-auto flex h-[87dvh] flex-col items-start justify-center gap-4 px-10 sm:px-20 md:mx-20"
+	class="mx-auto flex h-[85dvh] flex-col items-start justify-center gap-4 p-10 sm:px-20 md:mx-20"
 >
 	<section
-		class="mx-5 flex flex-col gap-4 text-justify text-base
+		class="mx-10 flex flex-col gap-4 text-justify
+		text-base
 		leading-relaxed
 		md:text-lg
-		lg:mx-40
-		xl:mx-65
-		2xl:mx-85"
+		lg:mx-20
+		xl:mx-45
+		2xl:mx-65"
 	>
 		<h1 class="text-4xl">Projects</h1>
-		<div>
-			<Card.Root>
-				<Card.Header>
-					<Card.Title>Personal Banking Dashboard</Card.Title>
-				</Card.Header>
-				<Card.Content>
-					<img alt="x" class="hidden md:block" />
-					<Card.Description
-						>A clean, responsive banking UI built with SvelteKit and Tailwind CSS. Inspired by
-						real-world finance apps, it features account overviews and transaction summaries.
-						Upcoming updates include dark mode, charts, and mock data to simulate a realistic
-						experience.</Card.Description
-					>
-				</Card.Content>
-				<Card.Footer class="grid grid-cols-2 items-start justify-center gap-1">
-					<a
-						href="https://ignatiusfairlight.github.io/bank-dashboard-demo/"
-						target="_blank"
-						class="col-span-2 text-blue-600 underline underline-offset-4 visited:text-purple-600"
-						>Live Website
-					</a>
-					<a
-						href="https://github.com/ignatiusfairlight/bank-dashboard-demo"
-						target="_blank"
-						class="text-blue-600 underline underline-offset-4 visited:text-purple-600"
-						>Github Link
-					</a>
-					<p class="self-center text-right">Status: In progress</p>
-				</Card.Footer>
-			</Card.Root>
-		</div>
-		<div>
-			<Card.Root>
-				<Card.Header>
-					<Card.Title>Event Scheduler</Card.Title>
-				</Card.Header>
-				<Card.Content>
-					<img alt="x" class="hidden md:block" />
-					<Card.Description
-						>A responsive event management web app built with SvelteKit, Tailwind CSS, and
-						shadcn-svelte, featuring an interactive calendar and a sortable, filterable data table.
-						Users can organize events by date, name, or location. Core functionality is complete,
-						with planned improvements including mobile responsiveness and user authentication.</Card.Description
-					>
-				</Card.Content>
-				<Card.Footer class="grid grid-cols-2 items-start justify-center gap-1">
-					<a
-						href="https://ignatiusfairlight.github.io/scheduler-demo/"
-						target="_blank"
-						class="col-span-2 text-blue-600 underline underline-offset-4 visited:text-purple-600"
-						>Live Website
-					</a>
-					<a
-						href="https://github.com/ignatiusfairlight/scheduler-demo"
-						target="_blank"
-						class="text-blue-600 underline underline-offset-4 visited:text-purple-600"
-						>Github Link
-					</a>
-					<p class="self-center text-right">Status: In progress</p>
-				</Card.Footer>
-			</Card.Root>
+		<div class="h-[70dvh] overflow-y-scroll pr-5">
+			<div class="mb-2">
+				<Card.Root>
+					<Card.Header>
+						<Card.Title>Personal Banking Dashboard</Card.Title>
+					</Card.Header>
+					<Card.Content>
+						<img alt="x" class="hidden md:block" />
+						<Card.Description
+							>A clean, responsive banking UI built with SvelteKit and Tailwind CSS. Inspired by
+							real-world finance apps, it features account overviews and transaction summaries.
+							Upcoming updates include dark mode, charts, and mock data to simulate a realistic
+							experience.</Card.Description
+						>
+					</Card.Content>
+					<Card.Footer class="grid grid-cols-2 items-start justify-center gap-1">
+						<a
+							href="https://ignatiusfairlight.github.io/bank-dashboard-demo/"
+							target="_blank"
+							class="col-span-2 text-blue-600 underline underline-offset-4 visited:text-purple-600"
+							>Live Website
+						</a>
+						<a
+							href="https://github.com/ignatiusfairlight/bank-dashboard-demo"
+							target="_blank"
+							class="text-blue-600 underline underline-offset-4 visited:text-purple-600"
+							>Github Link
+						</a>
+						<p class="self-center text-right">Status: In progress</p>
+					</Card.Footer>
+				</Card.Root>
+			</div>
+			<div class="mb-2">
+				<Card.Root>
+					<Card.Header>
+						<Card.Title>Event Scheduler</Card.Title>
+					</Card.Header>
+					<Card.Content>
+						<img alt="x" class="hidden md:block" />
+						<Card.Description
+							>A responsive event management web app built with SvelteKit, Tailwind CSS, and
+							shadcn-svelte, featuring an interactive calendar and a sortable, filterable data
+							table. Users can organize events by date, name, or location. Core functionality is
+							complete, with planned improvements including mobile responsiveness and user
+							authentication.</Card.Description
+						>
+					</Card.Content>
+					<Card.Footer class="grid grid-cols-2 items-start justify-center gap-1">
+						<a
+							href="https://ignatiusfairlight.github.io/scheduler-demo/"
+							target="_blank"
+							class="col-span-2 text-blue-600 underline underline-offset-4 visited:text-purple-600"
+							>Live Website
+						</a>
+						<a
+							href="https://github.com/ignatiusfairlight/scheduler-demo"
+							target="_blank"
+							class="text-blue-600 underline underline-offset-4 visited:text-purple-600"
+							>Github Link
+						</a>
+						<p class="self-center text-right">Status: In progress</p>
+					</Card.Footer>
+				</Card.Root>
+			</div>
+			<div class="mb-2">
+				<Card.Root>
+					<Card.Header>
+						<Card.Title>Event Scheduler</Card.Title>
+					</Card.Header>
+					<Card.Content>
+						<img alt="x" class="hidden md:block" />
+						<Card.Description
+							>A responsive event management web app built with SvelteKit, Tailwind CSS, and
+							shadcn-svelte, featuring an interactive calendar and a sortable, filterable data
+							table. Users can organize events by date, name, or location. Core functionality is
+							complete, with planned improvements including mobile responsiveness and user
+							authentication.</Card.Description
+						>
+					</Card.Content>
+					<Card.Footer class="grid grid-cols-2 items-start justify-center gap-1">
+						<a
+							href="https://ignatiusfairlight.github.io/scheduler-demo/"
+							target="_blank"
+							class="col-span-2 text-blue-600 underline underline-offset-4 visited:text-purple-600"
+							>Live Website
+						</a>
+						<a
+							href="https://github.com/ignatiusfairlight/scheduler-demo"
+							target="_blank"
+							class="text-blue-600 underline underline-offset-4 visited:text-purple-600"
+							>Github Link
+						</a>
+						<p class="self-center text-right">Status: In progress</p>
+					</Card.Footer>
+				</Card.Root>
+			</div>
+			<div class="mb-2">
+				<Card.Root>
+					<Card.Header>
+						<Card.Title>Event Scheduler</Card.Title>
+					</Card.Header>
+					<Card.Content>
+						<img alt="x" class="hidden md:block" />
+						<Card.Description
+							>A responsive event management web app built with SvelteKit, Tailwind CSS, and
+							shadcn-svelte, featuring an interactive calendar and a sortable, filterable data
+							table. Users can organize events by date, name, or location. Core functionality is
+							complete, with planned improvements including mobile responsiveness and user
+							authentication.</Card.Description
+						>
+					</Card.Content>
+					<Card.Footer class="grid grid-cols-2 items-start justify-center gap-1">
+						<a
+							href="https://ignatiusfairlight.github.io/scheduler-demo/"
+							target="_blank"
+							class="col-span-2 text-blue-600 underline underline-offset-4 visited:text-purple-600"
+							>Live Website
+						</a>
+						<a
+							href="https://github.com/ignatiusfairlight/scheduler-demo"
+							target="_blank"
+							class="text-blue-600 underline underline-offset-4 visited:text-purple-600"
+							>Github Link
+						</a>
+						<p class="self-center text-right">Status: In progress</p>
+					</Card.Footer>
+				</Card.Root>
+			</div>
 		</div>
 	</section>
 </main>
--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -6,25 +6,136 @@
 	class="mx-auto flex h-[87dvh] flex-col items-start justify-center gap-4 px-10 sm:px-20 md:mx-20"
 >
 	<section
-		class="flex flex-col gap-4 text-justify text-base leading-relaxed
-		mx-5
+		class="mx-10 flex flex-col gap-4 text-justify
+		text-base
+		leading-relaxed
 		md:text-lg
-		lg:mx-40
-		xl:mx-65
-		2xl:mx-85"
+		lg:mx-20
+		xl:mx-45
+		2xl:mx-65"
 	>
-	<h1 class="text-4xl mb-5">Resume</h1>
-		<Card.Root>
-			<Card.Header>
-				<Card.Title>Freelance Web Developer</Card.Title>
-			</Card.Header>
-			<Card.Content>
-				<Card.Description
-					>A clean, responsive banking UI built with SvelteKit and Tailwind CSS. Inspired by
-					real-world finance apps, it features account overviews and transaction summaries. Upcoming
-					updates include dark mode, charts, and mock data to simulate a realistic experience.</Card.Description
-				>
-			</Card.Content>
-		</Card.Root>
+		<h1 class="mb-5 text-4xl">Resume</h1>
+		<div class="h-[70dvh] overflow-y-scroll pr-5">
+			<div class="mb-2">
+				<Card.Root>
+					<Card.Header>
+						<Card.Title>Freelance Web Developer</Card.Title>
+					</Card.Header>
+					<Card.Content>
+						<Card.Description
+							>A clean, responsive banking UI built with SvelteKit and Tailwind CSS. Inspired by
+							real-world finance apps, it features account overviews and transaction summaries.
+							Upcoming updates include dark mode, charts, and mock data to simulate a realistic
+							experience.</Card.Description
+						>
+					</Card.Content>
+				</Card.Root>
+			</div>
+			<div class="mb-2">
+				<Card.Root>
+					<Card.Header>
+						<Card.Title>User Experience Analyst</Card.Title>
+					</Card.Header>
+					<Card.Content>
+						<Card.Description
+							>A clean, responsive banking UI built with SvelteKit and Tailwind CSS. Inspired by
+							real-world finance apps, it features account overviews and transaction summaries.
+							Upcoming updates include dark mode, charts, and mock data to simulate a realistic
+							experience.</Card.Description
+						>
+					</Card.Content>
+				</Card.Root>
+			</div>
+			<div class="mb-2">
+				<Card.Root>
+					<Card.Header>
+						<Card.Title>Intern, Operation Services</Card.Title>
+					</Card.Header>
+					<Card.Content>
+						<Card.Description
+							>A clean, responsive banking UI built with SvelteKit and Tailwind CSS. Inspired by
+							real-world finance apps, it features account overviews and transaction summaries.
+							Upcoming updates include dark mode, charts, and mock data to simulate a realistic
+							experience.</Card.Description
+						>
+					</Card.Content>
+				</Card.Root>
+			</div>
+			<div class="mb-2">
+				<Card.Root>
+					<Card.Header>
+						<Card.Title>Intern, Operation Services</Card.Title>
+					</Card.Header>
+					<Card.Content>
+						<Card.Description
+							>A clean, responsive banking UI built with SvelteKit and Tailwind CSS. Inspired by
+							real-world finance apps, it features account overviews and transaction summaries.
+							Upcoming updates include dark mode, charts, and mock data to simulate a realistic
+							experience.</Card.Description
+						>
+					</Card.Content>
+				</Card.Root>
+			</div>
+			<div class="mb-2">
+				<Card.Root>
+					<Card.Header>
+						<Card.Title>Intern, Operation Services</Card.Title>
+					</Card.Header>
+					<Card.Content>
+						<Card.Description
+							>A clean, responsive banking UI built with SvelteKit and Tailwind CSS. Inspired by
+							real-world finance apps, it features account overviews and transaction summaries.
+							Upcoming updates include dark mode, charts, and mock data to simulate a realistic
+							experience.</Card.Description
+						>
+					</Card.Content>
+				</Card.Root>
+			</div>
+			<div class="mb-2">
+				<Card.Root>
+					<Card.Header>
+						<Card.Title>Intern, Operation Services</Card.Title>
+					</Card.Header>
+					<Card.Content>
+						<Card.Description
+							>A clean, responsive banking UI built with SvelteKit and Tailwind CSS. Inspired by
+							real-world finance apps, it features account overviews and transaction summaries.
+							Upcoming updates include dark mode, charts, and mock data to simulate a realistic
+							experience.</Card.Description
+						>
+					</Card.Content>
+				</Card.Root>
+			</div>
+			<div class="mb-2">
+				<Card.Root>
+					<Card.Header>
+						<Card.Title>Intern, Operation Services</Card.Title>
+					</Card.Header>
+					<Card.Content>
+						<Card.Description
+							>A clean, responsive banking UI built with SvelteKit and Tailwind CSS. Inspired by
+							real-world finance apps, it features account overviews and transaction summaries.
+							Upcoming updates include dark mode, charts, and mock data to simulate a realistic
+							experience.</Card.Description
+						>
+					</Card.Content>
+				</Card.Root>
+			</div>
+			<div class="mb-2">
+				<Card.Root>
+					<Card.Header>
+						<Card.Title>Intern, Operation Services</Card.Title>
+					</Card.Header>
+					<Card.Content>
+						<Card.Description
+							>A clean, responsive banking UI built with SvelteKit and Tailwind CSS. Inspired by
+							real-world finance apps, it features account overviews and transaction summaries.
+							Upcoming updates include dark mode, charts, and mock data to simulate a realistic
+							experience.</Card.Description
+						>
+					</Card.Content>
+				</Card.Root>
+			</div>
+		</div>
 	</section>
 </main>
