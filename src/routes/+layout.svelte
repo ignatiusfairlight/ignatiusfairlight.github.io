e21ff02aa1cd6e6a6afcfea656face1352cb9c51
Bryan Erang Lawai <138428485+ignatiusfairlight@users.noreply.github.com>
2026-01-02 13:55:11 +0800

    Brushed up Resume page (need better description of my past positions).

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -99,5 +99,5 @@
 {@render children()}
 
 <footer class="flex flex-col items-center justify-center px-5">
-	<p>© 2026 Bryan Erang Lawai. All Rights Reserved.</p>
+	<p>© 2026 Ignatius Fairlight. All Rights Reserved.</p>
 </footer>
--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -6,7 +6,7 @@
 	class="mx-auto flex h-[86dvh] flex-col items-start justify-center gap-4 px-10 sm:px-20 md:mx-20"
 >
 	<section
-		class="mx-10 flex flex-col gap-4 text-justify
+		class="mx-10 flex flex-col text-justify
 		text-base
 		leading-relaxed
 		md:text-lg
@@ -19,120 +19,93 @@
 			<div class="mb-2">
 				<Card.Root>
 					<Card.Header>
-						<Card.Title>Freelance Web Developer</Card.Title>
+						<Card.Title class="flex flex-col">
+							<p class="text-2xl">Freelance Web Developer</p>
+							<p class="text-base">July 2024 - Present</p>
+						</Card.Title>
 					</Card.Header>
 					<Card.Content>
-						<Card.Description
-							>A clean, responsive banking UI built with SvelteKit and Tailwind CSS. Inspired by
-							real-world finance apps, it features account overviews and transaction summaries.
-							Upcoming updates include dark mode, charts, and mock data to simulate a realistic
-							experience.</Card.Description
-						>
+						<Card.Description>
+							<ul class="flex list-disc flex-col gap-2 px-5">
+								<li>
+									Designed and developed custom scheduling and web applications using Svelte,
+									TailwindCSS, and HTML for clients in the non-profit and small business sectors.
+								</li>
+								<li>
+									Built a custom volunteer scheduling web app for a local church to efficiently
+									manage weekly service duties and volunteer rosters, streamlining communication and
+									reducing manual coordination.
+								</li>
+								<li>
+									Developed responsive, user-friendly front-end interfaces with a focus on clear
+									user experience and mobile compatibility.
+								</li>
+								<li>
+									Collaborated directly with stakeholders to gather requirements, deliver
+									prototypes, and iterate based on feedback.
+								</li>
+							</ul>
+						</Card.Description>
 					</Card.Content>
 				</Card.Root>
 			</div>
 			<div class="mb-2">
 				<Card.Root>
 					<Card.Header>
-						<Card.Title>User Experience Analyst</Card.Title>
+						<Card.Title class="flex flex-col">
+							<p class="text-2xl">User Experience Analyst</p>
+							<p class="text-base">October 2023 - June 2024</p>
+						</Card.Title>
 					</Card.Header>
 					<Card.Content>
-						<Card.Description
-							>A clean, responsive banking UI built with SvelteKit and Tailwind CSS. Inspired by
-							real-world finance apps, it features account overviews and transaction summaries.
-							Upcoming updates include dark mode, charts, and mock data to simulate a realistic
-							experience.</Card.Description
-						>
+						<Card.Description>
+							<ul class="flex list-disc flex-col gap-2 px-5">
+								<li>
+									Analyzed survey data from up to 5,000 entries using advanced Excel functions to
+									extract actionable insights.
+								</li>
+								<li>
+									Produced interim and final reports summarizing user feedback trends and
+									recommending strategic improvements to enhance web services.
+								</li>
+								<li>
+									Conducted usability testing with tools like Google Lighthouse to assess and report
+									on performance and accessibility.
+								</li>
+								<li>
+									Facilitated user survey sessions and live walkthroughs for 10+ participants per
+									session, ensuring accurate data collection.
+								</li>
+							</ul>
+						</Card.Description>
 					</Card.Content>
 				</Card.Root>
 			</div>
 			<div class="mb-2">
 				<Card.Root>
 					<Card.Header>
-						<Card.Title>Intern, Operation Services</Card.Title>
+						<Card.Title class="flex flex-col">
+							<p class="text-2xl">Intern, Operation Serivces</p>
+							<p class="text-base">October 2022 - January 2023</p>
+						</Card.Title>
 					</Card.Header>
 					<Card.Content>
-						<Card.Description
-							>A clean, responsive banking UI built with SvelteKit and Tailwind CSS. Inspired by
-							real-world finance apps, it features account overviews and transaction summaries.
-							Upcoming updates include dark mode, charts, and mock data to simulate a realistic
-							experience.</Card.Description
-						>
-					</Card.Content>
-				</Card.Root>
-			</div>
-			<div class="mb-2">
-				<Card.Root>
-					<Card.Header>
-						<Card.Title>Intern, Operation Services</Card.Title>
-					</Card.Header>
-					<Card.Content>
-						<Card.Description
-							>A clean, responsive banking UI built with SvelteKit and Tailwind CSS. Inspired by
-							real-world finance apps, it features account overviews and transaction summaries.
-							Upcoming updates include dark mode, charts, and mock data to simulate a realistic
-							experience.</Card.Description
-						>
-					</Card.Content>
-				</Card.Root>
-			</div>
-			<div class="mb-2">
-				<Card.Root>
-					<Card.Header>
-						<Card.Title>Intern, Operation Services</Card.Title>
-					</Card.Header>
-					<Card.Content>
-						<Card.Description
-							>A clean, responsive banking UI built with SvelteKit and Tailwind CSS. Inspired by
-							real-world finance apps, it features account overviews and transaction summaries.
-							Upcoming updates include dark mode, charts, and mock data to simulate a realistic
-							experience.</Card.Description
-						>
-					</Card.Content>
-				</Card.Root>
-			</div>
-			<div class="mb-2">
-				<Card.Root>
-					<Card.Header>
-						<Card.Title>Intern, Operation Services</Card.Title>
-					</Card.Header>
-					<Card.Content>
-						<Card.Description
-							>A clean, responsive banking UI built with SvelteKit and Tailwind CSS. Inspired by
-							real-world finance apps, it features account overviews and transaction summaries.
-							Upcoming updates include dark mode, charts, and mock data to simulate a realistic
-							experience.</Card.Description
-						>
-					</Card.Content>
-				</Card.Root>
-			</div>
-			<div class="mb-2">
-				<Card.Root>
-					<Card.Header>
-						<Card.Title>Intern, Operation Services</Card.Title>
-					</Card.Header>
-					<Card.Content>
-						<Card.Description
-							>A clean, responsive banking UI built with SvelteKit and Tailwind CSS. Inspired by
-							real-world finance apps, it features account overviews and transaction summaries.
-							Upcoming updates include dark mode, charts, and mock data to simulate a realistic
-							experience.</Card.Description
-						>
-					</Card.Content>
-				</Card.Root>
-			</div>
-			<div class="mb-2">
-				<Card.Root>
-					<Card.Header>
-						<Card.Title>Intern, Operation Services</Card.Title>
-					</Card.Header>
-					<Card.Content>
-						<Card.Description
-							>A clean, responsive banking UI built with SvelteKit and Tailwind CSS. Inspired by
-							real-world finance apps, it features account overviews and transaction summaries.
-							Upcoming updates include dark mode, charts, and mock data to simulate a realistic
-							experience.</Card.Description
-						>
+						<Card.Description>
+							<ul class="flex list-disc flex-col gap-2 px-5">
+								<li>
+									Assisted in preparing monthly Value of Work Done (VOWD) reports to support project
+									ROI analysis and resource allocation strategies.
+								</li>
+								<li>
+									Maintained effective communication with supervisors and stakeholders to streamline
+									operational workflows.
+								</li>
+								<li>
+									Documented meeting minutes with precision to ensure accountability and track key
+									decisions across teams.
+								</li>
+							</ul>
+						</Card.Description>
 					</Card.Content>
 				</Card.Root>
 			</div>
