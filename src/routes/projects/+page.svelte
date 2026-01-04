5f203d454228f233775310aa38f3e86b97cb97d3
Bryan Erang Lawai <138428485+ignatiusfairlight@users.noreply.github.com>
2026-01-04 14:58:55 +0800

    H-Auto is better (for now)

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <main
-	class="mx-auto flex h-[95dvh] flex-col items-start justify-center gap-4 overflow-y-scroll px-10"
+	class="mx-auto mt-10 flex h-auto flex-col items-start justify-center gap-4 overflow-y-scroll px-10"
 >
 	<section
 		class="flex flex-col
@@ -12,134 +12,129 @@
 		xl:mx-45
 		2xl:mx-65"
 	>
-		<div class="h-[85dvh]">
-			<div class="mb-2">
-				<Card.Root>
-					<Card.Header>
-						<Card.Title class="text-lg lg:text-2xl">Personal Banking Dashboard</Card.Title>
-					</Card.Header>
-					<Card.Content>
-						<img alt="x" class="hidden md:block" />
-						<Card.Description class="text-justify text-sm">
-							A clean, responsive banking UI built with SvelteKit and Tailwind CSS. Inspired by
-							real-world finance apps, it features account overviews and transaction summaries.
-							Upcoming updates include dark mode, charts, and mock data to simulate a realistic
-							experience.
-						</Card.Description>
-					</Card.Content>
-					<Card.Footer class="grid grid-cols-2 items-start justify-center gap-1">
-						<a
-							href="https://ignatiusfairlight.github.io/bank-dashboard-demo/"
-							target="_blank"
-							class="col-span-2 text-blue-600 underline underline-offset-4 visited:text-purple-600"
-							>Live Website
-						</a>
-						<a
-							href="https://github.com/ignatiusfairlight/bank-dashboard-demo"
-							target="_blank"
-							class="text-blue-600 underline underline-offset-4 visited:text-purple-600"
-							>Github Link
-						</a>
-						<p class="self-center text-right">Status: In progress</p>
-					</Card.Footer>
-				</Card.Root>
-			</div>
-			<div class="mb-2">
-				<Card.Root>
-					<Card.Header>
-						<Card.Title class="text-lg lg:text-2xl">Event Scheduler</Card.Title>
-					</Card.Header>
-					<Card.Content>
-						<img alt="x" class="hidden md:block" />
-						<Card.Description class="text-justify text-sm">
-							A responsive event management web app built with SvelteKit, Tailwind CSS, and
-							shadcn-svelte, featuring an interactive calendar and a sortable, filterable data
-							table. Users can organize events by date, name, or location. Core functionality is
-							complete, with planned improvements including mobile responsiveness and user
-							authentication.
-						</Card.Description>
-					</Card.Content>
-					<Card.Footer class="grid grid-cols-2 items-start justify-center gap-1">
-						<a
-							href="https://ignatiusfairlight.github.io/scheduler-demo/"
-							target="_blank"
-							class="col-span-2 text-blue-600 underline underline-offset-4 visited:text-purple-600"
-							>Live Website
-						</a>
-						<a
-							href="https://github.com/ignatiusfairlight/scheduler-demo"
-							target="_blank"
-							class="text-blue-600 underline underline-offset-4 visited:text-purple-600"
-							>Github Link
-						</a>
-						<p class="self-center text-right">Status: In progress</p>
-					</Card.Footer>
-				</Card.Root>
-			</div>
-			<div class="mb-2">
-				<Card.Root>
-					<Card.Header>
-						<Card.Title class="text-lg lg:text-2xl">Event Scheduler</Card.Title>
-					</Card.Header>
-					<Card.Content>
-						<img alt="x" class="hidden md:block" />
-						<Card.Description class="text-justify text-sm">
-							A responsive event management web app built with SvelteKit, Tailwind CSS, and
-							shadcn-svelte, featuring an interactive calendar and a sortable, filterable data
-							table. Users can organize events by date, name, or location. Core functionality is
-							complete, with planned improvements including mobile responsiveness and user
-							authentication.
-						</Card.Description>
-					</Card.Content>
-					<Card.Footer class="grid grid-cols-2 items-start justify-center gap-1">
-						<a
-							href="https://ignatiusfairlight.github.io/scheduler-demo/"
-							target="_blank"
-							class="col-span-2 text-blue-600 underline underline-offset-4 visited:text-purple-600"
-							>Live Website
-						</a>
-						<a
-							href="https://github.com/ignatiusfairlight/scheduler-demo"
-							target="_blank"
-							class="text-blue-600 underline underline-offset-4 visited:text-purple-600"
-							>Github Link
-						</a>
-						<p class="self-center text-right">Status: In progress</p>
-					</Card.Footer>
-				</Card.Root>
-			</div>
-			<div class="mb-2">
-				<Card.Root>
-					<Card.Header>
-						<Card.Title>Event Scheduler</Card.Title>
-					</Card.Header>
-					<Card.Content>
-						<img alt="x" class="hidden md:block" />
-						<Card.Description class="text-justify text-sm"
-							>A responsive event management web app built with SvelteKit, Tailwind CSS, and
-							shadcn-svelte, featuring an interactive calendar and a sortable, filterable data
-							table. Users can organize events by date, name, or location. Core functionality is
-							complete, with planned improvements including mobile responsiveness and user
-							authentication.</Card.Description
-						>
-					</Card.Content>
-					<Card.Footer class="grid grid-cols-2 items-start justify-center gap-1">
-						<a
-							href="https://ignatiusfairlight.github.io/scheduler-demo/"
-							target="_blank"
-							class="col-span-2 text-blue-600 underline underline-offset-4 visited:text-purple-600"
-							>Live Website
-						</a>
-						<a
-							href="https://github.com/ignatiusfairlight/scheduler-demo"
-							target="_blank"
-							class="text-blue-600 underline underline-offset-4 visited:text-purple-600"
-							>Github Link
-						</a>
-						<p class="self-center text-right">Status: In progress</p>
-					</Card.Footer>
-				</Card.Root>
-			</div>
+		<div class="mb-2">
+			<Card.Root>
+				<Card.Header>
+					<Card.Title class="text-lg lg:text-2xl">Personal Banking Dashboard</Card.Title>
+				</Card.Header>
+				<Card.Content>
+					<img alt="x" class="hidden md:block" />
+					<Card.Description class="text-justify text-sm">
+						A clean, responsive banking UI built with SvelteKit and Tailwind CSS. Inspired by
+						real-world finance apps, it features account overviews and transaction summaries.
+						Upcoming updates include dark mode, charts, and mock data to simulate a realistic
+						experience.
+					</Card.Description>
+				</Card.Content>
+				<Card.Footer class="grid grid-cols-2 items-start justify-center gap-1">
+					<a
+						href="https://ignatiusfairlight.github.io/bank-dashboard-demo/"
+						target="_blank"
+						class="col-span-2 text-blue-600 underline underline-offset-4 visited:text-purple-600"
+						>Live Website
+					</a>
+					<a
+						href="https://github.com/ignatiusfairlight/bank-dashboard-demo"
+						target="_blank"
+						class="text-blue-600 underline underline-offset-4 visited:text-purple-600"
+						>Github Link
+					</a>
+					<p class="self-center text-right">Status: In progress</p>
+				</Card.Footer>
+			</Card.Root>
+		</div>
+		<div class="mb-2">
+			<Card.Root>
+				<Card.Header>
+					<Card.Title class="text-lg lg:text-2xl">Event Scheduler</Card.Title>
+				</Card.Header>
+				<Card.Content>
+					<img alt="x" class="hidden md:block" />
+					<Card.Description class="text-justify text-sm">
+						A responsive event management web app built with SvelteKit, Tailwind CSS, and
+						shadcn-svelte, featuring an interactive calendar and a sortable, filterable data table.
+						Users can organize events by date, name, or location. Core functionality is complete,
+						with planned improvements including mobile responsiveness and user authentication.
+					</Card.Description>
+				</Card.Content>
+				<Card.Footer class="grid grid-cols-2 items-start justify-center gap-1">
+					<a
+						href="https://ignatiusfairlight.github.io/scheduler-demo/"
+						target="_blank"
+						class="col-span-2 text-blue-600 underline underline-offset-4 visited:text-purple-600"
+						>Live Website
+					</a>
+					<a
+						href="https://github.com/ignatiusfairlight/scheduler-demo"
+						target="_blank"
+						class="text-blue-600 underline underline-offset-4 visited:text-purple-600"
+						>Github Link
+					</a>
+					<p class="self-center text-right">Status: In progress</p>
+				</Card.Footer>
+			</Card.Root>
+		</div>
+		<div class="mb-2">
+			<Card.Root>
+				<Card.Header>
+					<Card.Title class="text-lg lg:text-2xl">Event Scheduler</Card.Title>
+				</Card.Header>
+				<Card.Content>
+					<img alt="x" class="hidden md:block" />
+					<Card.Description class="text-justify text-sm">
+						A responsive event management web app built with SvelteKit, Tailwind CSS, and
+						shadcn-svelte, featuring an interactive calendar and a sortable, filterable data table.
+						Users can organize events by date, name, or location. Core functionality is complete,
+						with planned improvements including mobile responsiveness and user authentication.
+					</Card.Description>
+				</Card.Content>
+				<Card.Footer class="grid grid-cols-2 items-start justify-center gap-1">
+					<a
+						href="https://ignatiusfairlight.github.io/scheduler-demo/"
+						target="_blank"
+						class="col-span-2 text-blue-600 underline underline-offset-4 visited:text-purple-600"
+						>Live Website
+					</a>
+					<a
+						href="https://github.com/ignatiusfairlight/scheduler-demo"
+						target="_blank"
+						class="text-blue-600 underline underline-offset-4 visited:text-purple-600"
+						>Github Link
+					</a>
+					<p class="self-center text-right">Status: In progress</p>
+				</Card.Footer>
+			</Card.Root>
+		</div>
+		<div class="mb-2">
+			<Card.Root>
+				<Card.Header>
+					<Card.Title>Event Scheduler</Card.Title>
+				</Card.Header>
+				<Card.Content>
+					<img alt="x" class="hidden md:block" />
+					<Card.Description class="text-justify text-sm"
+						>A responsive event management web app built with SvelteKit, Tailwind CSS, and
+						shadcn-svelte, featuring an interactive calendar and a sortable, filterable data table.
+						Users can organize events by date, name, or location. Core functionality is complete,
+						with planned improvements including mobile responsiveness and user authentication.</Card.Description
+					>
+				</Card.Content>
+				<Card.Footer class="grid grid-cols-2 items-start justify-center gap-1">
+					<a
+						href="https://ignatiusfairlight.github.io/scheduler-demo/"
+						target="_blank"
+						class="col-span-2 text-blue-600 underline underline-offset-4 visited:text-purple-600"
+						>Live Website
+					</a>
+					<a
+						href="https://github.com/ignatiusfairlight/scheduler-demo"
+						target="_blank"
+						class="text-blue-600 underline underline-offset-4 visited:text-purple-600"
+						>Github Link
+					</a>
+					<p class="self-center text-right">Status: In progress</p>
+				</Card.Footer>
+			</Card.Root>
 		</div>
 	</section>
 </main>
