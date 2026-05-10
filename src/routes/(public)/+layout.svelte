c3299e13b913db0b72d1acfbb191fe54b4d97fee
Ignatius Fairlight <lawaierang@gmail.com>
2026-05-10 13:42:56 +0800

    feat: - added layout.css and navbar on both sides

--- src/routes/(public)/+layout.svelte
+++ src/routes/(public)/+layout.svelte
@@ -1,9 +1,24 @@
 <script lang="ts">
 	import './layout.css';
+	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from "flowbite-svelte";
 	import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<Navbar>
+  <NavBrand href="/">
+    <img src="/images/flowbite-svelte-icon-logo.svg" class="me-3 h-6 sm:h-9" alt="Flowbite Logo" />
+    <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+  </NavBrand>
+  <NavHamburger />
+  <NavUl>
+    <NavLi href="/">Home</NavLi>
+    <NavLi href="/about">About</NavLi>
+    <NavLi href="/docs/components/navbar">Navbar</NavLi>
+    <NavLi href="/pricing">Pricing</NavLi>
+    <NavLi href="/contact">Contact</NavLi>
+  </NavUl>
+</Navbar>
 {@render children()}
